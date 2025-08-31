2757d6013de473f77008d45624488158a69dcf4d
Galuh Wasesa <surat.sesha@gmail.com>
2025-09-01 04:50:54 +0700

    feat: user login route

--- src/modules/auth/route.ts
+++ src/modules/auth/route.ts
@@ -1,6 +1,6 @@
 import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 import { prisma } from "../../lib/prisma";
-import { AuthRegisterSchema } from "./schema";
+import { AuthLoginSchema, AuthRegisterSchema } from "./schema";
 import { PrivateUserSchema } from "../user/schema";
 
 export const authRoute = new OpenAPIHono();
@@ -30,6 +30,37 @@ authRoute.openapi(
         fullName: body.fullName,
       },
     });
+    return c.json(user, 201);
+  }
+);
+
+authRoute.openapi(
+  createRoute({
+    method: "post",
+    path: "/login",
+    request: {
+      body: {
+        content: { "application/json": { schema: AuthLoginSchema } },
+      },
+    },
+    responses: {
+      200: {
+        content: { "application/json": { schema: PrivateUserSchema } },
+        description: "Login Success",
+      },
+      404: { description: "User Not Found" },
+    },
+  }),
+
+  async (c) => {
+    const body = c.req.valid("json");
+    const user = await prisma.user.findUnique({
+      where: { email: body.email },
+    });
+    if (!user) {
+      return c.notFound();
+    }
+
     return c.json(user);
   }
 );
