33212aec5227276cf02b72696d59bac35db34476
Galuh Wasesa <surat.sesha@gmail.com>
2025-09-01 05:06:30 +0700

    feat:  prepare auth me route

--- src/modules/auth/route.ts
+++ src/modules/auth/route.ts
@@ -64,3 +64,27 @@ authRoute.openapi(
     return c.json(user);
   }
 );
+
+authRoute.openapi(
+  createRoute({
+    method: "post",
+    path: "/me",
+
+    responses: {
+      200: {
+        content: { "application/json": { schema: PrivateUserSchema } },
+        description: "Login Success",
+      },
+      404: { description: "User Not Found" },
+    },
+  }),
+
+  async (c) => {
+    const user = await prisma.user.findFirst();
+    if (!user) {
+      return c.notFound();
+    }
+
+    return c.json(user);
+  }
+);
